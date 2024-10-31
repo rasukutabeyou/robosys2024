6c828c9f4ce90231c8b2c8ebb0396b672fcc269b
Kouta Sakai <K14sakai@gmail.com>
2024-10-31 12:43:17 +0900

    D

--- test.bash
+++ test.bash
@@ -7,8 +7,19 @@ ng () {
 
 res=0
 
+###通常###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+###以上###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ] || ng "LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 
 [ "${res}" = 0 ] && echo OK
 exit $res
