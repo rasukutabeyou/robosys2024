751605bea60fa6bc82da8b2d5e8f9d5fa91e0a06
Kouta Sakai <K14sakai@gmail.com>
2024-11-23 15:55:14 +0900

    kakuninn

--- test.bash
+++ test.bash
@@ -13,13 +13,13 @@ out=$(seq 5 | ./plus)
 
 ###以上###
 out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng "LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo  | ./plus)
-[ "$?" = 1 ] || ng "LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 
-[ "${res}" = 0 ] && echo OK
+[ "$res" = 0 ] && echo OK
 exit $res
