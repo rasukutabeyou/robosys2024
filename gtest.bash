b27072de0947e51cc1cba0ba4d30e1c12ce9f9dc
Kouta Sakai <K14sakai@gmail.com>
2024-11-23 19:45:42 +0900

    Gtest ver1

--- gtest.bash
+++ gtest.bash
@@ -1,1 +1,15 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Kouta Sakai
+# SPDX-License-Identifier: BSD-3-Clause
+
+wng () {
+         echo warnig${1}行目
+	 res=1
+}
+
+res=0
+
+out=$(./genngou 2005 | tr -d '[:space:]')
+[ "${out}" = "2005年は平成17年" ] || wng "$LINENO"
+
+exit $res
