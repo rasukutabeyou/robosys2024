d4f8d4240cfd3cba8141e42043ff6d0ba7a2aeaf
Kouta Sakai <K14sakai@gmail.com>
2024-11-24 17:28:01 +0900

    Gtest ver4

--- gtest.bash
+++ gtest.bash
@@ -11,51 +11,51 @@ res=0
 
 #正常動作
 
-out=$(./genngou 2024 year | tr -d '[:space:]')
-[ "${out}" = "2024年は令和6年" ] || wng "$LINENO"
+out=$(echo -e "2024\nyear" | ./genngou | tr -d '[:space:]')
+[ "${out}" = "value:mode:2024年は令和6年" ] || wng "$LINENO"
 
-out=$(./genngou 2005  year | tr -d '[:space:]')
-[ "${out}" = "2005年は平成17年" ] || wng "$LINENO"
+out=$(echo -e "2005\nyear" | ./genngou 2005 | tr -d '[:space:]')
+[ "${out}" = "value:mode:2005年は平成17年" ] || wng "$LINENO"
 
-out=$(./genngou 1980  year | tr -d '[:space:]')
-[ "${out}" = "1980年は昭和55年" ] || wng "$LINENO"
+out=$(echo -e "1980\nyear" | ./genngou 1980 | tr -d '[:space:]')
+[ "${out}" = "value:mode:1980年は昭和55年" ] || wng "$LINENO"
 
-out=$(./genngou 2019  year | tr -d '[:space:]')
-[ "${out}" = "2019年は令和元年（平成31年)" ] || wng "$LINENO"
+out=$(echo -e "2019\nyear" | ./genngou 2019 | tr -d '[:space:]')
+[ "${out}" = "value:mode:2019年は令和元年（平成31年)" ] || wng "$LINENO"
 
-out=$(./genngou 1989  year | tr -d '[:space:]')
-[ "${out}" = "1989年は平成元年(昭和64年)" ] || wng "$LINENO"
+out=$(echo -e "1989\nyear" | ./genngou 1989 | tr -d '[:space:]')
+[ "${out}" = "value:mode:1989年は平成元年(昭和64年)" ] || wng "$LINENO"
 
-out=$(./genngou 1926  year | tr -d '[:space:]')
-[ "${out}" = "1926年は昭和元年(大正15年)" ] || wng "$LINENO"
+out=$(echo -e "1926\nyear" | ./genngou 1926 | tr -d '[:space:]')
+[ "${out}" = "value:mode:1926年は昭和元年(大正15年)" ] || wng "$LINENO"
 
-out=$(./genngou 1 age | tr -d '[:space:]')
-[ "${out}" = "1歳は令和5年生まれ" ] || wng "$LINENO"
+out=$(echo -e "1\nage" | ./genngou | tr -d '[:space:]')
+[ "${out}" = "value:mode:1歳は令和5年生まれ" ] || wng "$LINENO"
 
-out=$(./genngou 5 age | tr -d '[:space:]')
-[ "${out}" = "5歳は令和元年（平成31年)生まれ" ] || wng "$LINENO"
+out=$(echo -e "5\nage" | ./genngou | tr -d '[:space:]')
+[ "${out}" = "value:mode:5歳は令和元年（平成31年)生まれ" ] || wng "$LINENO"
 
-out=$(./genngou 19 age | tr -d '[:space:]')
-[ "${out}" = "19歳は平成17年生まれ" ] || wng "$LINENO"
+out=$(echo -e "19\nage" | ./genngou | tr -d '[:space:]')
+[ "${out}" = "value:mode:19歳は平成17年生まれ" ] || wng "$LINENO"
 
-out=$(./genngou 35 age | tr -d '[:space:]')
-[ "${out}" = "35歳は平成元年(昭和64年)生まれ" ] || wng "$LINENO"
+out=$(echo -e "35\nage" | ./genngou | tr -d '[:space:]')
+[ "${out}" = "value:mode:35歳は平成元年(昭和64年)生まれ" ] || wng "$LINENO"
 
-out=$(./genngou 46 age | tr -d '[:space:]')
-[ "${out}" = "46歳は昭和53年生まれ" ] || wng "$LINENO"
+out=$(echo -e "46\nage" | ./genngou | tr -d '[:space:]')
+[ "${out}" = "value:mode:46歳は昭和53年生まれ" ] || wng "$LINENO"
 
-out=$(./genngou 98 age | tr -d '[:space:]')
-[ "${out}" = "98歳は昭和元年(大正15年)生まれ" ] || wng "$LINENO"
+out=$(echo -e "98\nage" | ./genngou | tr -d '[:space:]')
+[ "${out}" = "value:mode:98歳は昭和元年(大正15年)生まれ" ] || wng "$LINENO"
 
 #異常動作
 
-out=$(./genngou あ)
+out=$(echo -e "ちくわ\n天ぷら" | ./genngou)
 [ "$?" = 1 ] || wng "$LINENO"
-[ "${out}" = "" ] || wng "$LINENO"
+[ "${out}" = "value:mode:" ] || wng "$LINENO"
 
-out=$(./genngou )
+out=$(echo -e " \n " | ./genngou )
 [ "$?" = 1 ] || wng "$LINENO"
-[ "${out}" = "" ] || wng "$LINENO"
+[ "${out}" = "value:mode:" ] || wng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 
